b40d1290c39eb56f98055a80564a598d0d80df8c
Takato56 <lan.05062003@gmail.com>
2026-04-15 17:25:53 +0700

    Update project

--- Controller/LoginController.cpp
+++ Controller/LoginController.cpp
@@ -4,22 +4,24 @@ LoginResult LoginController::login() {
     LoginResult result;
     StaffRepo sr(db);
 
+    std::string phoneNumber;
+    std::cout << "Enter phone number: ";
+    std::cin >> phoneNumber;
+
+    Employee emp = sr.getByPhone(phoneNumber);
+
     std::string pin;
     std::cout << "Enter PIN: ";
     std::cin >> pin;
 
-    std::vector<Employee> employees = sr.getAll();
 
-    for (const auto& emp : employees) {
-        if (PasswordHasher::verify(pin, emp.getPinHash())) {
-            result.isSuccess = true;
-            result.staffId = emp.getId();
-            result.role = emp.getRole();
-            result.empName = emp.getName();
-            return result;
-        }
+    if (PasswordHasher::verify(pin, emp.getPinHash())) {
+        result.isSuccess = true;
+        result.staffId = emp.getId();
+        result.role = emp.getRole();
+        result.empName = emp.getName();
+        return result;
     }
 
-    std::cout << "Login failed!\n";
     return result;
 }
